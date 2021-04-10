38306880e7b3e47acf60308052c5a671446c02fd
Anton Bachin <antonbachin@yahoo.com>
2021-04-10 08:30:52 +0300

    Tweak API docs

--- src/dream.mli
+++ src/dream.mli
@@ -44,8 +44,7 @@ and middleware = handler -> handler
       let () =
         Dream.run
         @@ Dream.logger
-        @@ fun _ ->
-          Dream.respond "Good morning, world!"
+        @@ fun _ -> Dream.respond "Good morning, world!"
     ]}
 
     Examples
