126cdf1e60eb5a191f88a305239d4ecffce1cd2a
Raphael Rafatpanah <persianturtle@gmail.com>
2021-04-19 16:16:08 -0400

    Add single-binary example

---
+++ example/w-one-binary/one_binary.ml
@@ -0,0 +1,12 @@
+let loader _ path _request =
+  match Assets.read path with
+  | None -> Dream.empty `Not_Found
+  | Some asset -> Dream.respond asset
+
+let () =
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/assets/**" (Dream.static ~loader "")
+  ]
+  @@ Dream.not_found
