9246cc4bdc4312f482855f56ecdf20cab7b42855
Anton Bachin <antonbachin@yahoo.com>
2021-05-11 20:13:25 +0300

    Tweak license headers

--- src/middleware/catch.ml
+++ src/middleware/catch.ml
@@ -1,5 +1,5 @@
-(* This file is part of Dream, released under the MIT license. See
-   LICENSE.md for details, or visit https://github.com/aantron/dream.
+(* This file is part of Dream, released under the MIT license. See LICENSE.md
+   for details, or visit https://github.com/aantron/dream.
 
    Copyright 2021 Anton Bachin *)
 
--- test/expect/middleware/initialize.ml
+++ test/expect/middleware/initialize.ml
@@ -1,2 +1,9 @@
+(* This file is part of Dream, released under the MIT license. See LICENSE.md
+   for details, or visit https://github.com/aantron/dream.
+
+   Copyright 2021 Anton Bachin *)
+
+
+
 let require : unit =
   Dream.initialize_log ~enable:false ()
