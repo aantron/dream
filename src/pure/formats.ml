73e96380fb6a0e396fa7110313fa78b2f9631ca1
Calascibetta Romain <romain.calascibetta@gmail.com>
2021-07-03 17:51:46 +0200

    Fix assert false and mapping between number and month

--- src/pure/formats.ml
+++ src/pure/formats.ml
@@ -64,9 +64,9 @@ let to_set_cookie
       let ((y, m, d), ((hh, mm, ss), _tz_offset_s)) = Ptime.to_date_time time in
       let month =
         match m with
-        | 0 -> "Jan" | 1 -> "Feb" | 2 -> "Mar" | 3 -> "Apr" | 4 -> "May"
-        | 5 -> "Jun" | 6 -> "Jul" | 7 -> "Aug" | 8 -> "Sep" | 9 -> "Oct"
-        | 10 -> "Nov" | 11 -> "Dec"
+        | 1 -> "Jan" | 2 -> "Feb" | 3 -> "Mar" | 4 -> "Apr" | 5 -> "May"
+        | 6 -> "Jun" | 7 -> "Jul" | 8 -> "Aug" | 9 -> "Sep" | 10 -> "Oct"
+        | 11 -> "Nov" | 12 -> "Dec"
         | _ -> assert false
       in
       (* [Ptime.to_date_time] docs give range 0..60 for [ss], accounting for leap
