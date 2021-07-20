47f4ed505624239ad356ac898c1b45d075ad87c7
Joe Thomas <jsthomas@protonmail.com>
2021-07-20 09:26:34 -0600

    Tidy up flash message cookies (#137)
    Fixes #134.

--- src/middleware/flash.ml
+++ src/middleware/flash.ml
@@ -26,12 +26,17 @@ let flash_messages inner_handler request =
   let request = Dream.with_local storage outbox request in
   let%lwt response = inner_handler request in
   let entries = List.rev !outbox in
+  let existing = Dream.cookie flash_cookie request in
+  let resp = match existing, entries with
+  | None, [] -> response
+  | Some _, [] -> Dream.set_cookie flash_cookie "" request response ~expires:0.
+  | _, _ ->
   let content =
     List.fold_right (fun (x,y) a -> `String x :: `String y :: a) entries [] in
   let value = `List content |> Yojson.Basic.to_string in
   Dream.set_cookie flash_cookie value request response ~max_age:five_minutes
-  |> Lwt.return
-
+  in
+  Lwt.return resp
 
 
 let (|>?) =
