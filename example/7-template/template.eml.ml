edfed4b57056922014e76bf916bb9c649ff49868
Anton <antonoutkine@gmail.com>
2021-08-02 21:52:14 -0500

    Make template example more complex
    Include if/match statement and for loop. Use different syntax styles to
    illustrate the range of possibilities.

--- example/7-template/template.eml.ml
+++ example/7-template/template.eml.ml
@@ -1,19 +1,55 @@
-let render param =
+(* In OCaml, `begin ... end` is the same as `( ... )` *)
+let render_home tasks =
   <html>
   <body>
-    <h1>The URL parameter was <%s param %>!</h1>
+    <h1>My TODO</h1>
+    <% tasks |> List.iter begin fun (name, complete) -> %>
+      <p>Task <%s name %>:
+        <% if complete then ( %>
+          complete!
+        <% ) else ( %>
+          not complete 
+        <% ); %>
+      </p>
+    <% end; %>
   </body>
   </html>
+
+
+(* You can also begin a line with `%` instead of using `<% ... %>` *)
+let render_task tasks task =
+  <html>
+  <body>
+%   (match List.find_opt (fun (task_, _) -> task = task_) tasks with 
+%   | Some (name, complete) -> 
+      <h1>TODO task: <%s name %>, complete: <%B complete %></h1>
+%   | None -> begin
+      <h1>Task not found!</h1>
+%   end);
+  </body>
+  </html>
+
+let tasks = [
+  ("write documentation", true);
+  ("create examples", true);
+  ("publish website", true);
+  ("profit", false);
+] 
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router [
+    Dream.get "/"
+      (fun _ ->
+        render_home tasks
+        |> Dream.html);
+        
 
-    Dream.get "/:word"
+    Dream.get "/:task"
       (fun request ->
-        Dream.param "word" request
-        |> render
+        Dream.param "task" request
+        |> render_task tasks
         |> Dream.html);
 
   ]
--- example/r-template/template.eml.re
+++ example/r-template/template.eml.re
@@ -1,18 +1,50 @@
-let greet = who => {
+let render_home = tasks => {
   <html>
   <body>
-    <h1>Good morning, <%s who %>!</h1>
+    <h1>My TODO</h1>
+    <% tasks |> List.iter(((name, complete)) => { %>
+      <p>Task <%s name %>:
+        <% if (complete) { %>
+          complete!
+        <% } else { %>
+          not complete 
+        <% }; %>
+      </p>
+    <% }); %>
   </body>
   </html>
 };
+
+
+// You can begin a line with `%` instead of using `<% ... %>`
+let render_task = (tasks, task) => {
+  <html>
+  <body>
+%   (switch (List.find_opt(((task_, _)) => task == task_, tasks)) {
+%   | Some((name, complete)) => 
+      <h1>TODO task: <%s name %>, complete: <%B complete %></h1>
+%   | None =>
+      <h1>Task not found!</h1>
+%   });
+  </body>
+  </html>
+};
+
+let tasks = [
+  ("write documentation", true),
+  ("create examples", true),
+  ("publish website", true),
+  ("profit", false),
+];
 
 let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.router([
-
-    Dream.get("/",
-      (_ => Dream.html(greet("world")))),
-
+    Dream.get("/", _ => render_home(tasks) |> Dream.html),
+    Dream.get("/:task", request =>
+      Dream.param("task", request) |> render_task(tasks) |> Dream.html
+    ),
   ])
   @@ Dream.not_found;
+
