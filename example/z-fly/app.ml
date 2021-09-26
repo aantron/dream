aedc1f62f0cd0b396a6987dad67f9aca9e26067e
Dennis Dang <dangggdennis@gmail.com>
2021-09-25 21:23:23 -0700

    Copy over docker example with esy

---
+++ example/z-fly/app.ml
@@ -0,0 +1,7 @@
+let () =
+  Dream.run ~interface:"0.0.0.0" ~port:(int_of_string (Sys.getenv "PORT"))
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/" (fun _ -> Dream.html "Dream running in Heroku!");
+  ]
+  @@ Dream.not_found
