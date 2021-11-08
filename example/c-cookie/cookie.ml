41e036eee9ffaa8d84b1dd5570ee7cbbbf965849
Anton Bachin <antonbachin@yahoo.com>
2021-11-08 14:50:29 +0300

    Don't feature drop_cookie in the existing example

--- example/c-cookie/cookie.ml
+++ example/c-cookie/cookie.ml
@@ -1,20 +1,15 @@
 let () =
   Dream.run ~secret:"foo"
   @@ Dream.logger
-  @@ Dream.router [
-    Dream.get "/" (fun request ->
-      match Dream.cookie "ui.language" request with
-      | Some value ->
-        Printf.ksprintf
-          Dream.response "Your preferred language is %s! Your cookie has been removed." (Dream.html_escape value)
-        |> Dream.drop_cookie "ui.language" request
-        |> Lwt.return
+  @@ fun request ->
 
-      | None ->
-        Dream.response "Set language preference; come again!"
-        |> Dream.add_header "Content-Type" Dream.text_html
-        |> Dream.set_cookie "ui.language" "ut-OP" request
-        |> Lwt.return
-      )
-  ]
-  @@ Dream.not_found
+    match Dream.cookie "ui.language" request with
+    | Some value ->
+      Printf.ksprintf
+        Dream.html "Your preferred language is %s!" (Dream.html_escape value)
+
+    | None ->
+      Dream.response "Set language preference; come again!"
+      |> Dream.add_header "Content-Type" Dream.text_html
+      |> Dream.set_cookie "ui.language" "ut-OP" request
+      |> Lwt.return
