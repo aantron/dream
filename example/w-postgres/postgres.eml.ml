4294547f39ecf2bbf65c7e870b2870a36d9dacb6
Anton Bachin <antonbachin@yahoo.com>
2023-03-01 15:48:12 +0300

    Adapt PostgreSQL example to recent Caqti
    See recent commits for details.

--- example/w-postgres/postgres.eml.ml
+++ example/w-postgres/postgres.eml.ml
@@ -4,16 +4,18 @@ module T = Caqti_type
 
 let list_comments =
   let query =
-    R.collect T.unit T.(tup2 int string)
-      "SELECT id, text FROM comment" in
+    let open Caqti_request.Infix in
+    (T.unit ->* T.(tup2 int string))
+    "SELECT id, text FROM comment" in
   fun (module Db : DB) ->
     let%lwt comments_or_error = Db.collect_list query () in
     Caqti_lwt.or_fail comments_or_error
 
 let add_comment =
   let query =
-    R.exec T.string
-      "INSERT INTO comment (text) VALUES ($1)" in
+    let open Caqti_request.Infix in
+    (T.string ->. T.unit)
+    "INSERT INTO comment (text) VALUES ($1)" in
   fun text (module Db : DB) ->
     let%lwt unit_or_error = Db.exec query text in
     Caqti_lwt.or_fail unit_or_error
