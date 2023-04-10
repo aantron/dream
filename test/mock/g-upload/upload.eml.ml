1f7f9b95645c398389dba7ccff49716c55c29e8b
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-04-10 21:26:52 +0200

    Add mock for g-upload

---
+++ test/mock/g-upload/upload.eml.ml
@@ -0,0 +1,41 @@
+let home request =
+  <html>
+  <body>
+    <form method="POST" action="/" enctype="multipart/form-data">
+      <%s! Dream.csrf_tag request %>
+      <input name="files" type="file" multiple>
+      <button>Submit!</button>
+    </form>
+  </body>
+  </html>
+
+let report files =
+  <html>
+  <body>
+%   files |> List.iter begin fun (name, content) ->
+%     let name =
+%       match name with
+%       | None -> "None"
+%       | Some name -> name
+%     in
+      <p><%s name %>, <%i String.length content %> bytes</p>
+%   end;
+  </body>
+  </html>
+
+let () =
+  Eio_main.run @@ fun env ->
+  Dream.run env
+  @@ Dream.logger
+  @@ Dream.memory_sessions
+  @@ Dream.router [
+
+    Dream.get  "/" (fun request ->
+      Dream.html (home request));
+
+    Dream.post "/" (fun request ->
+      match Dream.multipart request with
+      | `Ok ["files", files] -> Dream.html (report files)
+      | _ -> Dream.empty `Bad_Request);
+
+  ]
