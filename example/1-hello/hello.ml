8dddedf32ff960b7bcd8234b479e8f2d0515dcd3
Anton Bachin <antonbachin@yahoo.com>
2024-10-10 23:34:37 +0300

    Attempting to reproduce #347 locally

--- example/1-hello/hello.ml
+++ example/1-hello/hello.ml
@@ -1,3 +1,12 @@
 let () =
-  Dream.run (fun _ ->
-    Dream.html "Good morning, world!")
+  Lwt_engine.set (new Lwt_engine.select);
+
+  Dream.router
+    [
+      Dream.get "/" (fun _ ->
+          Lwt.return
+            (Dream.response
+               ~headers:[ ("Content-Type", "text/plain") ]
+               "On macOS I leak.\n"));
+    ]
+  |> Dream.logger |> Dream.run ~port:8080
