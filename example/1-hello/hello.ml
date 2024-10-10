40f78617ef7040f3680b6776267f143ad317f202
Anton Bachin <antonbachin@yahoo.com>
2024-10-10 23:40:52 +0300

    Reproduce #347 in CI

--- example/1-hello/hello.ml
+++ example/1-hello/hello.ml
@@ -1,9 +1,13 @@
 let () =
   Lwt_engine.set (new Lwt_engine.select);
 
+  print_endline "example started, stdout";
+  prerr_endline "example started, stderr";
+
   Dream.router
     [
       Dream.get "/" (fun _ ->
+          prerr_endline "get";
           Lwt.return
             (Dream.response
                ~headers:[ ("Content-Type", "text/plain") ]
